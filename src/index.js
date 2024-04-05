fda37e3deeae96814b42bd2d13b840a7a3c0e6c0
parth <parthprajapati467@gmail.com>
2024-04-05 09:29:21 -0400

    Home page with adding functionality

--- src/index.js
+++ src/index.js
@@ -3,13 +3,36 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
+// const react = require("react");
+// const cors = require("cors");
+
+// const corsOptions = {
+//   origin: "http://localhost:3000",
+// };
+
+// const app = express();
+// app.use(cors(corsOptions));
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path='/*' element={<App />}>
+
+      </Route>
+    </Routes>
+    
+    </BrowserRouter>
+    
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
